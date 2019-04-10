fcf00dd4e3886f9b3fd6ae87cd1ad29c768f7069
Shawn <wangcong26@hotmail.com>
2019-04-10 12:09:39 -0400

    Update BinaryTree Preorder Traversal.java

--- BinaryTree Preorder Traversal.java
+++ BinaryTree Preorder Traversal.java
@@ -19,14 +19,14 @@ class Solution
      	// Make a list of integer
 	    List<Integer> myList =  new LinkedList<Integer>();
 		
-		// If the root is null, return myList.
+	// If the root is null, return myList.
         if(root==null)
             return myList;
 			
-		// Add the integer to myList
+	// Add the integer to myList
         myList.add(root.val);
 		
-		// Below is recursive way to add all the integers.
+	// Below is recursive way to add all the integers.
         myList.addAll(preorderTraversal(root.left));
         myList.addAll(preorderTraversal(root.right));
         return myList;
